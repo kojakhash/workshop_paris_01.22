a6132e8e2e084282d0ce57ab3a00701ffe12fa27
kojakhash <kojak.abdullazoda@hashicorp.com>
2022-01-25 18:55:10 +0100

    Modified code for kv lecture

--- index.js
+++ index.js
@@ -3,23 +3,32 @@ const express = require('express')
 const app = express()
 const port = 3000
 const Vault = require('hashi-vault-js');
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 
 app.get('/', async (req, res) => {
     var roleId = process.env.ROLE_ID
     var secretId = process.env.SECRET_ID
     var vaultURL = process.env.VAULT_ADDR
-    const vault = new Vault({
+    const approle = new Vault({
         https: false,
         baseUrl: `${vaultURL}/v1`,
-        rootPath: 'auth/approle',
+        rootPath: 'auth/api_node',
         timeout: 5000,
         proxy: false,
         namespace: 'admin'
     });
-    const token = await vault.loginWithAppRole(roleId, secretId);
-    const secrets = await vault.readKVSecret(token.client_token, "internal-api");
-    res.send({ "hello": secrets})
+    const kv = new Vault({
+        https: false,
+        baseUrl: `${vaultURL}/v1`,
+        rootPath: 'internal',
+        timeout: 5000,
+        proxy: false,
+        namespace: 'admin'
+    });
+    const token = await approle.loginWithAppRole(roleId, secretId);
+    const secrets = await kv.readKVSecret(token.client_token, "api");
+    res.send({ secrets })
 })
 
 app.listen(port, () => {
